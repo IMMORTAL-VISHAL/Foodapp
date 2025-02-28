dcac780978deb5119e456644842bc3c104078545
vishal pathak <vishalpathak842@gmail.com>
2025-03-01 00:18:01 +0530

    Add cart functionality and styling; integrate cart context in Navbar and Cart components

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,42 +1,51 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
- const StoreContextProvider = (props)=>{
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const [cartItems,setCartItems] = useState({});
-
-const addToCart =(itemId)=>{
-    if(!cartItems[itemId]){
-        setCartItems((prev)=>({...prev,[itemId]:1}))
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    else{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+  const getTotatlCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
-}
+    return totalAmount;
+  };
 
-const removeFromCart = (itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-}
-
-const contextValue = {
+  const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
-    removeFromCart
-}
+    removeFromCart,
+    getTotatlCartAmount,
+  };
+  // this is only for cheking the data will fetched perfectly or not
+  // useEffect(()=>{
+  //     console.log(cartItems);
 
-useEffect(()=>{
-    console.log(cartItems);
-    
-},[cartItems])
+  // },[cartItems])
 
-return (
+  return (
     <StoreContext.Provider value={contextValue}>
-        {props.children}
+      {props.children}
     </StoreContext.Provider>
-)
-}
+  );
+};
 export default StoreContextProvider;
