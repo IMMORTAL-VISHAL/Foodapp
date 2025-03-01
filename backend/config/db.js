6368c7c156860bd579d5bd404c9bd49bb1bb14d5
vishal pathak <vishalpathak842@gmail.com>
2025-03-01 12:49:32 +0530

    Refactor database connection logic in db.js to use environment variables and improve error handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://vishalpathak842:Vishalpathak842@cluster0.e7a0c.mongodb.net/FoodAppTomato').then(()=>console.log('DB CONNECTED'));
-}
+// export const connectDB = async()=>{
+//     await mongoose.connect('mongodb+srv://vishalpathak842:Vishalpathak842@cluster0.e7a0c.mongodb.net/FoodAppTomato').then(()=>console.log('DB CONNECTED'));
+// }
 // const connectDB = async () => {
 //     try {
 //         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
@@ -16,16 +16,16 @@ export const connectDB = async()=>{
 
 // export default connectDB
 
-// this is working and understand this what is happening in this 
-//  export const connectDB = () => {
-//     mongoose.connect(process.env.MONGODB_URL,{
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true
-//     })
-//     .then(()=>{
-//         console.log(`Database connected Successfully`);
-//     })
-//     .catch((error)=>{
-//         console.log(`Unable to connect to database due to ${error.message}`);
-//     })
-// }
+//this is working and understand this what is happening in this 
+ export const connectDB = () => {
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{
+        console.log(`Database connected Successfully`);
+    })
+    .catch((error)=>{
+        console.log(`Unable to connect to database due to ${error.message}`);
+    })
+}
