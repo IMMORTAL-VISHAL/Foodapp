e961bdeef7d1c26dd01b953de0a9c177df5e5663
vishal pathak <vishalpathak842@gmail.com>
2025-03-05 00:08:41 +0530

    Add MyOrders route to App for user order management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,21 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LogInPopUp from "./components/LogInPopUp/LogInPopUp";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
-  const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
-    {showLogin?<LogInPopUp setShowLogin={setShowLogin}/>:<></>}
+      {showLogin ? <LogInPopUp setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
-          <Route path="/verify" element={<Verify/>}/>
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
